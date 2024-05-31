15238d2da5808a2ec2355dc43996a1657ecf502a
BiTech5 <bikikc7@gmail.com>
2024-05-31 16:45:59 +0545

    updating the model

--- todoapp/models.py
+++ todoapp/models.py
@@ -5,3 +5,5 @@ class Todo(models.Model):
     user=models.ForeignKey(User,on_delete=models.CASCADE)
     task=models.CharField(max_length=100)
     status=models.BooleanField(default=False)
+    def __str__(self):
+        return self.task
