f48e5365e3412853db429a6550d742d8dbba5c26
BiTech5 <bikikc7@gmail.com>
2024-05-31 15:09:20 +0545

    register and login page with todo list in templates

--- todoapp/urls.py
+++ todoapp/urls.py
@@ -3,6 +3,7 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path('',views.home,name='home'),
-    path('login/',views.login,name='login')
+    path('login/',views.login,name='login'),
+    path('register',views.register,name='register')
 
 ]
--- todoapp/views.py
+++ todoapp/views.py
@@ -6,3 +6,6 @@ def home(request):
 
 def login(request):
     return render(request,'login.html')
+
+def register(request):
+    return render(request,'register.html')
